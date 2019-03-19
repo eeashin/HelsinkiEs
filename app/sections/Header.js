9073738272caf4ca1ec912e701c6e00ac27ae2a4
Eashin Matubber <eashin@gmail.com>
2019-03-19 08:18:51 +0200

    firebase datastore used

--- app/sections/Header.js
+++ app/sections/Header.js
@@ -2,54 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View, Image, AsyncStorage, Alert } from 'react-native';
 
 export class Header extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoggedIn: false,
-            loggedUser: false
-        };
-    }
+    
 
-    toggleUser = () => {
-        if (this.state.isLoggedIn) {
-            AsyncStorage.setItem('userLoggedIn', 'none', (err, result) => {
-                this.setState({
-                    isLoggedIn: false,
-                    loggedUser: false
-                });
-                Alert.alert('User logged out');
-            })
-
-        }
-        else {
-            this.props.navigate('LoginRT')
-        }
-    }
-
-    componentDidMount() {
-        AsyncStorage.getItem('userLoggedIn', (err, result) => {
-            if (result === 'none') {
-                console.log('NONE');
-            }
-            else if (result === null) {
-                AsyncStorage.setItem('userLoggedIn', 'none', (err, result) => {
-                    console.log('Set user to NONE');
-                })
-            }
-            else {
-                this.setState({
-                    isLoggedIn: true,
-                    loggedUser: result
-                });
-            }
-
-        })
-
-    }
+   
 
 
     render() {
-        let display = this.state.isLoggedIn ? this.state.loggedUser : this.props.message;
+       
         return (
             <View style={styles.headStyle}>
                 <Image
@@ -57,8 +16,8 @@ export class Header extends React.Component {
                     source={require('./img/SUS.png')}
                 />
                 <Text
-                    style={styles.headText}
-                    onPress={this.toggleUser}>{display}
+                    style={styles.headText}>
+
                 </Text>
             </View>
         );
--- app/views/firebaseconfig.js
+++ app/views/firebaseconfig.js
@@ -4,10 +4,13 @@ const firebaseConfig = {
   apiKey: "AIzaSyC5tD70NNdMUWd1BhOAjEmMW9NCRTkFrtk",
   authDomain: "hh-mobile-877f2.firebaseapp.com",
   databaseURL: "https://hh-mobile-877f2.firebaseio.com",
+  projectId: "hh-mobile-877f2",
   storageBucket: "hh-mobile-877f2.appspot.com",
+  messagingSenderId: "955085767397"
 };
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
+
 
 
 
